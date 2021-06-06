cddcc9d1d1ec3461988d03728c4b13280565c2f2
yuang <958102068@qq.com>
2021-06-06 17:02:58 +0800

    一版可用的

--- app.py
+++ app.py
@@ -1,7 +1,58 @@
 from flask import Flask
+from rcon import MCRcon
+
+from threading import Thread
+import time
+import threading
+import json
+
+lock = threading.Lock()
 
 app = Flask(__name__)
+class status:
+    def __init__(self):
+        self.online:bool = False
+        self.player_num = 0
+
+resp = status()
+
+def clock_thread():
+    global resp
+    print("clock thread start")
+    while True:
+        print("clock exec")
+        lock.acquire()
+        print("locked")
+        try:
+            with MCRcon("localhost", "111111q.", 25575) as mcr:
+                print("connection open")
+                res = mcr.command("/list")
+                print("online")
+                resp.online = True
+        except Exception:
+            resp.online = False
+            print("offline")
+        lock.release()
+        print("lock released")
+        time.sleep(10)
+
 
 @app.route("/api/status")
 def hello_world():
-    return "!23"
+    global resp
+    res = {
+        "status":resp.online,
+        "player_num":resp.player_num,
+    }
+    # return "online" if resp.online else "offline"
+    print(res)
+    return res
+
+
+if __name__== '__main__':
+    web = Thread(target=lambda:app.run(host="0.0.0.0",port="3000"))
+    #app.run(host="0.0.0.0",port="3000")
+    clock=Thread(target=clock_thread)
+    web.start()
+    clock.start()
+
---
+++ start.sh
@@ -0,0 +1,4 @@
+source ./venv/bin/activate
+export FLASK_APP=app
+# flask run --host=0.0.0.0 --port=3000
+python3 app.py
